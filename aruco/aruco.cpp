b13248b91de0af258dcb9f385600b324456fc029
Szymon Rajca <szrajca@hotmail.com>
2019-11-11 22:04:07 +0100

    code beautify

--- aruco/aruco.cpp
+++ aruco/aruco.cpp
@@ -15,7 +15,7 @@ std::vector<float> velocity(cv::Point2f point, float distance, float windowWidth
 	result.push_back(kp * errorLinear + kd * derivativeLinear);
 	previousErrorLinear = errorLinear;
 
-	float errorAngular = windowWidth/2.0 - point.x; // center of a scene
+	float errorAngular = windowWidth / 2.0 - point.x; // center of a scene
 	float derivativeAngular = (errorAngular - previousErrorAngular) / 0.1;
 	result.push_back(kp * errorAngular + kd * derivativeAngular);
 	previousErrorAngular = errorAngular;
@@ -24,22 +24,20 @@ std::vector<float> velocity(cv::Point2f point, float distance, float windowWidth
 }
 
 cv::Point2f coordinatesOfMarker(
-		const std::vector<std::vector<cv::Point2f>> &corners) {
+    const std::vector<std::vector<cv::Point2f>> &corners) {
 	cv::Point2f coordinates(
-			(corners[0][0].x + corners[0][1].x + corners[0][2].x
-					+ corners[0][3].x) / 4,
-			(corners[0][0].y + corners[0][1].y + corners[0][2].y
-					+ corners[0][3].y) / 4);
+	    (corners[0][0].x + corners[0][1].x + corners[0][2].x + corners[0][3].x) / 4,
+	    (corners[0][0].y + corners[0][1].y + corners[0][2].y + corners[0][3].y) / 4);
 	return coordinates;
 }
 
-const cv::String keys = 
-	"{id | 0 | Camera id }"
-	"{v  |   | Input from video file }";
+const cv::String keys =
+    "{id | 0 | Camera id }"
+    "{v  |   | Input from video file }";
 
 int main(int argc, char *argv[]) {
 	cv::CommandLineParser parser(argc, argv, keys);
-	parser.about("Object tracking using a ArUco marker");
+	parser.about("Object tracking using an ArUco marker");
 
 	int camId = parser.get<int>("id");
 
@@ -48,7 +46,7 @@ int main(int argc, char *argv[]) {
 		video = parser.get<cv::String>("v");
 	}
 
-	if(!parser.check()) {
+	if (!parser.check()) {
 		parser.printErrors();
 		return 0;
 	}
@@ -65,9 +63,9 @@ int main(int argc, char *argv[]) {
 	}
 
 	cv::Ptr<cv::aruco::DetectorParameters> detectorParams =
-			cv::aruco::DetectorParameters::create();
+	    cv::aruco::DetectorParameters::create();
 	cv::Ptr<cv::aruco::Dictionary> dictionary =
-			cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+	    cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
 
 	cv::Mat camMatrix, distCoeffs;
 	int totalIterations = 0;
@@ -80,24 +78,24 @@ int main(int argc, char *argv[]) {
 		std::vector<std::vector<cv::Point2f> > corners, rejected;
 
 		cv::aruco::detectMarkers(image, dictionary, corners, ids,
-				detectorParams, rejected);
+		                         detectorParams, rejected);
 		image.copyTo(imageCopy);
 
 		if (ids.size() > 0) {
 			cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-			float height = sqrt(
-					pow(abs(corners[0][0].x - corners[0][1].x), 2) +
-					pow(abs(corners[0][0].y - corners[0][1].y), 2));
+			float height = sqrt(pow(abs(corners[0][0].x - corners[0][1].x), 2) +
+			                    pow(abs(corners[0][0].y - corners[0][1].y), 2));
 			cv::Point2f coordinates = coordinatesOfMarker(corners);
 			std::vector<float> vels = velocity(coordinates, height,
-					inputVideo.get(cv::CAP_PROP_FRAME_WIDTH));
+			                                   inputVideo.get(cv::CAP_PROP_FRAME_WIDTH));
 			std::cout << vels[0] << " " << vels[1] << std::endl;
 		}
 
 		cv::imshow("out", imageCopy);
 		char key = (char) cv::waitKey(waitTime);
-		if (key == 27)
+		if (key == 27) {
 			break;
+		}
 	}
 
 	return 0;
